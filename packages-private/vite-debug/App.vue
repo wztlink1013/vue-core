92118c3fb44d94920870643ec73c868c0ace4664
wuzt <wzt@differsoft.com>
2024-09-25 17:24:39 +0800

    feat: dev backup

--- packages-private/vite-debug/App.vue
+++ packages-private/vite-debug/App.vue
@@ -1,4 +1,14 @@
 <template>
+  <div style="border: 1px solid #ccc">
+    <div style="color: #ccc">父组件</div>
+    <button @click="changeToHelloWorld">
+      父组件改变传入子组件的prop值：{{ toHelloWorld }}
+    </button>
+    <hello-world :toHelloWorld="toHelloWorld"></hello-world>
+    <div style="display: block;">computed toHelloWorldComputed value: {{ toHelloWorldComputed }}</div>
+  </div>
+
+  <hr style="margin: 50px 0 500px 0" />
   <h4>commit component</h4>
   <template v-for="branch in branches">
     <input
@@ -12,16 +22,17 @@
   </template>
   <p>vuejs/core@{{ currentBranch }}</p>
   <ul>
-    <li v-for="{ html_url, sha, author, commit } in (commits as any)">
-      <a :href="html_url" target="_blank" class="commit"
-        >{{ sha.slice(0, 7) }}</a
-      >
-      - <span class="message">{{ truncate(commit.message) }}</span><br />
+    <li v-for="{ html_url, sha, author, commit } in commits as any">
+      <a :href="html_url" target="_blank" class="commit">{{
+        sha.slice(0, 7)
+      }}</a>
+      - <span class="message">{{ truncate(commit.message) }}</span
+      ><br />
       by
       <span class="author"
-        ><a :href="author.html_url" target="_blank"
-          >{{ commit.author.name }}</a
-        ></span
+        ><a :href="author.html_url" target="_blank">{{
+          commit.author.name
+        }}</a></span
       >
       at <span class="date">{{ formatDate(commit.author.date) }}</span>
     </li>
@@ -30,55 +41,71 @@
 
 <script lang="ts">
 const API_URL = `https://api.github.com/repos/vuejs/core/commits?per_page=3&sha=`
+import HelloWorld from './HelloWorld.vue'
 
 export default {
-  data: () => ({
+  name: 'App',
+  components: { 'hello-world': HelloWorld },
+  data(a, b) {
+    console.info('data options', a, b)
+    return {
       branches: ['main', 'v2-compat'],
       currentBranch: 'main',
       commits: null,
-    }),
-
-    created() {
-      this.fetchData()
+      toHelloWorld: 'To Hello World message',
+    }
+  },
+  computed: {
+    toHelloWorldComputed() {
+      return `>>> ${this.toHelloWorld} compunted... <<<`
     },
+  },
 
-    watch: {
-      currentBranch: 'fetchData',
-    },
+  created() {
+    this.fetchData()
+  },
 
-    methods: {
-      fetchData() {
-        fetch(`${API_URL}${this.currentBranch}`)
-          .then(res => res.json())
-          .then(data => {
-            this.commits = data
-          })
-      },
-      truncate(v: string) {
-        const newline = v.indexOf('\n')
-        return newline > 0 ? v.slice(0, newline) : v
-      },
-      formatDate(v: string) {
-        return v.replace(/T|Z/g, ' ')
-      },
+  watch: {
+    currentBranch: 'fetchData',
+  },
+
+  methods: {
+    changeToHelloWorld() {
+      this.toHelloWorld = this.toHelloWorld + '!'
     },
+    fetchData() {
+      fetch(`${API_URL}${this.currentBranch}`)
+        .then(res => res.json())
+        .then(data => {
+          this.commits = data
+        })
+    },
+    truncate(v: string) {
+      const newline = v.indexOf('\n')
+      return newline > 0 ? v.slice(0, newline) : v
+    },
+    formatDate(v: string) {
+      return v.replace(/T|Z/g, ' ')
+    },
+  },
 }
+console.info('>>> vue >>>')
 </script>
 
 <style scoped>
-  #demo {
-    font-family: 'Helvetica', Arial, sans-serif;
-  }
-  a {
-    text-decoration: none;
-    color: #f66;
-  }
-  li {
-    line-height: 1.5em;
-    margin-bottom: 20px;
-  }
-  .author,
-  .date {
-    font-weight: bold;
-  }
+#demo {
+  font-family: 'Helvetica', Arial, sans-serif;
+}
+a {
+  text-decoration: none;
+  color: #f66;
+}
+li {
+  line-height: 1.5em;
+  margin-bottom: 20px;
+}
+.author,
+.date {
+  font-weight: bold;
+}
 </style>
---
+++ packages-private/vite-debug/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div style="border: 1px pink solid;margin: 10px;">
+    <div style="color: pink;">HelloWorld组件</div>
+    <div>toHelloWorld: {{ toHelloWorld }}</div>
+    <button @click="changeToHelloWorld">⛔子组件改变父组件传入的prop会报错</button>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'HelloWorld',
+  props: ['toHelloWorld'],
+  methods: {
+    changeToHelloWorld() {
+      this.toHelloWorld = `子组件改变了父组件传入的prop${new Date()}`
+    }
+  },
+}
+</script>
